ed50169a803d8ec72f3c1265451c5a1d05158f39
flzl2008 <goddlaek@naver.com>
2018-05-31 16:36:55 +0900

    chore: 이미지 파일 업로드 경로 변경
    - TradeBoardController.java: 이미지 경로를 하드코딩에서 함수에 리턴되는 값으로 변경

--- src/main/java/org/kosta/momsbay/controller/TradeBoardController.java
+++ src/main/java/org/kosta/momsbay/controller/TradeBoardController.java
@@ -37,7 +37,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/trade")
 @Controller
 public class TradeBoardController {
-	private String uploadPath = "C:/java-kosta/framework-workspace2/resources/upload/postImg/";
+	private String uploadPath = "/resources/upload/postImg/";
 	@Resource
 	private CommentService commentService;
 	@Resource
@@ -144,7 +144,7 @@ public class TradeBoardController {
 			try {
 				UUID uid = UUID.randomUUID();
 				savedName = uid.toString().substring(0, 5) + "_" + multifile.getOriginalFilename();
-				File target = new File(uploadPath, savedName);
+				File target = new File(session.getServletContext().getRealPath("/")+uploadPath, savedName);
 				FileCopyUtils.copy(multifile.getBytes(), target);
 				tradePostService.addTradePostPhoto(savedName, tradePostVO.getTradePostNo());
 			} catch (IOException e) {
@@ -213,7 +213,7 @@ public class TradeBoardController {
 			try {
 				UUID uid = UUID.randomUUID();
 				savedName = uid.toString().substring(0, 5) + "_" + multifile.getOriginalFilename();
-				File target = new File(uploadPath, savedName);
+				File target = new File(session.getServletContext().getRealPath("/")+uploadPath, savedName);
 				FileCopyUtils.copy(multifile.getBytes(), target);
 				tradePostService.updateTradePostPhoto(savedName, tradePostVO.getTradePostNo());
 			} catch (IOException e) {
@@ -301,7 +301,7 @@ public class TradeBoardController {
 			try {
 				UUID uid = UUID.randomUUID();
 				savedName = uid.toString().substring(0, 5) + "_" + multifile.getOriginalFilename();
-				File target = new File(uploadPath, savedName);
+				File target = new File(session.getServletContext().getRealPath("/")+uploadPath, savedName);
 				FileCopyUtils.copy(multifile.getBytes(), target);
 				sharePostService.addSharePostPhoto(savedName, sharePostVO.getNoneTradePostNo());
 			} catch (IOException e) {
@@ -358,7 +358,7 @@ public class TradeBoardController {
 			try {
 				UUID uid = UUID.randomUUID();
 				savedName = uid.toString().substring(0, 5) + "_" + multifile.getOriginalFilename();
-				File target = new File(uploadPath, savedName);
+				File target = new File(session.getServletContext().getRealPath("/")+uploadPath, savedName);
 				FileCopyUtils.copy(multifile.getBytes(), target);
 				sharePostService.updateSharePostPhoto(savedName, sharePostVO.getNoneTradePostNo());
 			} catch (IOException e) {
